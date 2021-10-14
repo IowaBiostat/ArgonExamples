aaa0c0dd02d6ef1631737d7e18d8c375d2c04f2c
Grant D Brown <gown@argon-itf-login-3.hpc>
2021-10-14 11:50:15 -0500

    Update example 03

--- 03_Simple_Job_With_Args/submitSeveralJobs.R
+++ 03_Simple_Job_With_Args/submitSeveralJobs.R
@@ -1,5 +1,5 @@
 for (i in 1:10){
   print(i)
-  system(paste0("./submit_job.sh --output=", paste0("output_", i, ".txt")), intern = FALSE)
+  system(paste0("./submit_job.sh ", paste0("output_", i, ".txt")), intern = FALSE)
 }
 
