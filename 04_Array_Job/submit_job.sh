f9595908c62dd6dbd7aef7d692956ffe096c5817
Grant D Brown <gown@argon-itf-login-3.hpc>
2021-10-14 11:52:40 -0500

    Tweak example 4

--- 04_Array_Job/submit_job.sh
+++ 04_Array_Job/submit_job.sh
@@ -2,6 +2,6 @@
 # "-pe smp 2": Single threaded program, just request 2 slot to get one full core
 # "-cwd": Run this job in the current working directory
 # "-t 1-20" run 20 of these jobs (array)
-qsub -pe smp 2 -cwd -t 1-20 simple_args.job
+qsub -pe smp 2 -cwd -e /dev/null -o /dev/null -t 1-20 simple_args.job
 
 
