738ae5a6cba5397f8f81e8c01afe22bb43916db3
Grant D Brown <gown@argon-itf-login-4.hpc>
2021-10-26 20:31:25 -0500

    Switch to using XML in sge_interface.R

--- Scripts/sge_interface.R
+++ Scripts/sge_interface.R
@@ -1,28 +1,23 @@
-
+library(XML)
 
 get_jobs <- function(uid){
-	job_rslt <- system(paste0("qstat -u ", uid), intern = TRUE)
-	
-	if (length(job_rslt) <= 2){
-		return(data.frame(`job-ID`=integer(0), 
-		                  prior=numeric(0),
-		                  name=character(0), 
-		                  user=character(0), 
+	job_rslt <- xmlToList(system(paste0("qstat -u ", uid, " -xml"), intern = TRUE))
+ 		
+	if (length(names(job_rslt$queue_info)) == 0){
+		return(data.frame(jobID=integer(0), 
 		                  state=character(0), 
-		                  `submit/start`=character(0), 
-		                  at=character(0), 
+		                  submitted=character(0), 
 		                  queue=character(0), 
-		                  slots=character(0), 
-		                  `ja-task-ID`=integer(0)))
+		                  slots=character(0)))
 	} else{
-	  field_names <- setdiff(strsplit(gsub("^ *|(?<= ) | *$", "", job_rslt[1], perl = TRUE), split = " ")[[1]], "at")
-		out <- as.data.frame(Reduce("rbind", lapply(job_rslt[3:length(job_rslt)], function(jr){
-		  matrix(strsplit(gsub("^ *|(?<= ) | *$", "", jr, perl = TRUE), split = " ")[[1]], nrow=1)
-		})))
-		colnames(out) <- field_names
-		out$`job-ID` <- as.integer(out$`job-ID`)
-		out$prior <- as.numeric(out$prior)
-		out$`ja-task-ID` <- as.integer(out$`ja-task-ID`)
-	}
-  out
+
+
+	return(Reduce("rbind", lapply(1:length(names(job_rslt$queue_info)), function(i){
+		data.frame(jobID = job_rslt$queue_info[[i]]$JB_job_number, 
+			   state = job_rslt$queue_info[[i]]$state, 
+			   submitted = job_rslt$queue_info[[i]]$JAT_start_time,
+                           queue =  job_rslt$queue_info[[i]]$queue_name,
+                           slots =  job_rslt$queue_info[[i]]$slots)
+	})))
+    
 }
