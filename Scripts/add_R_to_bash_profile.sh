96af7702a4c4ae2d6f4182d2d6458f22e8127a41
Grant D Brown <gown@argon-login-2.hpc>
2021-11-03 11:40:46 -0500

    Modify R install scripts to use stack/2021.1

--- Scripts/add_R_to_bash_profile.sh
+++ Scripts/add_R_to_bash_profile.sh
@@ -6,7 +6,11 @@ CWD=$(pwd)
 touch ~/.bash_profile
 # Update path
 echo "PATH=\$PATH:$CWD/$RVERS/bin" >>  ~/.bash_profile
-echo 'module load python/3.7.0' >> ~/.bash_profile
-echo 'module load gcc/8.3.0' >> ~/.bash_profile
+echo 'module load stack/2021.1' >> ~/.bash_profile
+echo 'module load gcc/9.3.0' >> ~/.bash_profile
+echo 'module load python/3.8.8_gcc-9.3.0' >> ~/.bash_profile
+echo 'module load pcre2'>> ~/.bash_profile
+echo 'module load curl'>> ~/.bash_profile
+
 echo 'export PATH' >> ~/.bash_profile
 #
