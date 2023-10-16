d200af2e59d07d6db7a8f56a16768c7fb6bcee54
Grant Brown <grant.brown73@gmail.com>
2023-10-16 14:29:59 -0500

    Update Julia version and module dependency

--- Scripts/get_julia.sh
+++ Scripts/get_julia.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-JVERS=julia-1.6.3
-wget https://julialang-s3.julialang.org/bin/linux/x64/1.6/$JVERS-linux-x86_64.tar.gz
+JVERS=julia-1.9.3
+wget https://julialang-s3.julialang.org/bin/linux/x64/1.9/$JVERS-linux-x86_64.tar.gz
 tar -xzf $JVERS-linux-x86_64.tar.gz
 
 CWD=$(pwd)
@@ -8,7 +8,6 @@ CWD=$(pwd)
 touch ~/.bash_profile
 # Update path
 echo "PATH=\$PATH:$CWD/$JVERS/bin" >>  ~/.bash_profile
-echo 'module load python/3.7.0' >> ~/.bash_profile
-echo 'module load gcc/8.3.0' >> ~/.bash_profile
+echo 'module load stack/2022.2' >> ~/.bash_profile
 echo 'export PATH' >> ~/.bash_profile
 
